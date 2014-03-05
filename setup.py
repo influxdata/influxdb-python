d756f5e87d63d8d45253dbf8bab1f2db1591d33f
Bruno Binet <bruno.binet@gmail.com>
2014-03-05 14:33:47 +0100

    add python trove classifiers to setup.py

--- setup.py
+++ setup.py
@@ -36,4 +36,16 @@ setup(
     tests_require=test_requires,
     install_requires=requires,
     extras_require={'test': test_requires},
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ),
 )
