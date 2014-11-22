aa0803f90db510327515546208e955e9652d6c6d
Tim Tröndle <troendle@younicos.com>
2014-11-22 20:37:20 +0100

    Fixed list series

--- tests/influxdb/misc_test.py
+++ tests/influxdb/misc_test.py
@@ -143,3 +143,16 @@ class TestDataFrameClient(unittest.TestCase):
             cli = DataFrameClient('host', 8086, 'username', 'password', 'db')
             result = cli.query('select column_one from foo;')
             assert_frame_equal(dataframe, result)
+
+    def test_list_series(self):
+        response = [
+            {
+                'columns': ['time', 'name'],
+                'name': 'list_series_result',
+                'points': [[0, 'seriesA'], [0, 'seriesB']]
+            }
+        ]
+        with _mocked_session('get', 200, response):
+            cli = DataFrameClient('host', 8086, 'username', 'password', 'db')
+            series_list = cli.get_list_series()
+            assert series_list == ['seriesA', 'seriesB']
