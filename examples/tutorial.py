8bb255100fd7a7724b9a18741e9020c50c564645
aviau <alexandre.viau@savoirfairelinux.com>
2015-02-28 13:50:20 -0500

    Updated tutorial.py for 0.9.0

--- examples/tutorial.py
+++ examples/tutorial.py
@@ -9,35 +9,25 @@ def main(host='localhost', port=8086):
     dbname = 'example'
     dbuser = 'smly'
     dbuser_password = 'my_secret_password'
-    query = 'select column_one from foo;'
-    json_body = [{
-        "points": [
-            ["1", 1, 1.0],
-            ["2", 2, 2.0]
-        ],
-        "name": "foo",
-        "columns": ["column_one", "column_two", "column_three"]
-    }]
+    query = 'select value from cpu_load_short;'
+    json_body = [
+        {
+            "name": "cpu_load_short",
+            "tags": {
+                "host": "server01",
+                "region": "us-west"
+            },
+            "timestamp": "2009-11-10T23:00:00Z",
+            "fields": {
+                "value": 0.64
+            }
+        }
+    ]
 
     client = InfluxDBClient(host, port, user, password, dbname)
 
     print("Create database: " + dbname)
     client.create_database(dbname)
-
-    dbusers = client.get_database_users()
-    print("Get list of database users: {0}".format(dbusers))
-
-    print("Add database user: " + dbuser)
-    client.add_database_user(dbuser, dbuser_password)
-
-    print("Make user a database admin")
-    client.set_database_admin(dbuser)
-
-    print("Remove admin privilege from user")
-    client.unset_database_admin(dbuser)
-
-    dbusers = client.get_database_users()
-    print("Get list of database users again: {0}".format(dbusers))
 
     print("Switch user: " + dbuser)
     client.switch_user(dbuser, dbuser_password)
@@ -54,7 +44,7 @@ def main(host='localhost', port=8086):
     client.switch_user(user, password)
 
     print("Delete database: " + dbname)
-    client.delete_database(dbname)
+    client.drop_database(dbname)
 
 
 def parse_args():
