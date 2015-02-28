ab3e3bff0ad72da893c3ba025e6de8cd8e1f797d
aviau <alexandre.viau@savoirfairelinux.com>
2015-02-28 17:21:30 -0500

    SeriesHelper: Added tags support

--- examples/tutorial_serieshelper.py
+++ examples/tutorial_serieshelper.py
@@ -8,20 +8,22 @@ class MySeriesHelper(SeriesHelper):
         client = InfluxDBClient()
         # The client should be an instance of InfluxDBClient.
         series_name = 'events.stats.{server_name}'
-        # The series name must be a string. Add dependent field names in curly brackets.
-        fields = ['time', 'server_name']
+        # The series name must be a string. Add dependent fields/tags in curly brackets.
+        fields = ['some_stat']
         # Defines all the fields in this time series.
+        tags = ['server_name']
+        # Defines all the tags for the series.
         bulk_size = 5
         # Defines the number of data points to store prior to writing on the wire.
 
 # The following will create *five* (immutable) data points.
 # Since bulk_size is set to 5, upon the fifth construction call, *all* data
 # points will be written on the wire via MySeriesHelper.Meta.client.
-MySeriesHelper(server_name='us.east-1', time=159)
-MySeriesHelper(server_name='us.east-1', time=158)
-MySeriesHelper(server_name='us.east-1', time=157)
-MySeriesHelper(server_name='us.east-1', time=156)
-MySeriesHelper(server_name='us.east-1', time=155)
+MySeriesHelper(server_name='us.east-1', some_stat=159)
+MySeriesHelper(server_name='us.east-1', some_stat=158)
+MySeriesHelper(server_name='us.east-1', some_stat=157)
+MySeriesHelper(server_name='us.east-1', some_stat=156)
+MySeriesHelper(server_name='us.east-1', some_stat=155)
 
 # To manually submit data points which are not yet written, call commit:
 MySeriesHelper.commit()
