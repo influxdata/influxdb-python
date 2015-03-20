17602b256758e4130daf7fd56c0cca85f8487d35
aviau <alexandre.viau@savoirfairelinux.com>
2015-03-20 13:39:06 -0400

    Fixed bad string format syntax

--- influxdb/client.py
+++ influxdb/client.py
@@ -325,7 +325,7 @@ class InfluxDBClient(object):
         query_string = \
             "CREATE RETENTION POLICY %s ON %s " \
             "DURATION %s REPLICATION %s" % \
-            (name, database or self._database, duration, replication)
+            (name, (database or self._database), duration, replication)
 
         if default is True:
             query_string += " DEFAULT"
@@ -337,14 +337,14 @@ class InfluxDBClient(object):
         Get the list of retention policies
         """
         return self.query(
-            "SHOW RETENTION POLICIES %s" % database or self._database
+            "SHOW RETENTION POLICIES %s" % (database or self._database)
         )
 
     def get_list_series(self, database=None):
         """
         Get the list of series
         """
-        return self.query("SHOW SERIES", database=database or self._database)
+        return self.query("SHOW SERIES", database=(database or self._database))
 
     def get_list_users(self):
         """
