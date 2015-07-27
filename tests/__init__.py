cd8a2cef18ea627e673e00efa125471aa7d60b13
aviau <alexandre.viau@savoirfairelinux.com>
2015-07-26 22:18:06 -0400

    Ensure tests pass without influxdb bin

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 import unittest
 import sys
+import os
 
 using_pypy = hasattr(sys, "pypy_version_info")
 skipIfPYpy = unittest.skipIf(using_pypy, "Skipping this test on pypy.")
+
+_skip_server_tests = os.environ.get('INFLUXDB_PYTHON_SKIP_SERVER_TESTS', None) == 'True'
+skipServerTests = unittest.skipIf(_skip_server_tests, "Skipping server tests...")
--- tests/influxdb/server_tests/client_test_with_server.py
+++ tests/influxdb/server_tests/client_test_with_server.py
@@ -11,17 +11,11 @@ This basically duplicates what's in client_test.py
 """
 
 from __future__ import print_function
-import datetime
-import distutils.spawn
+
 from functools import partial
 import os
-import shutil
-import subprocess
-import sys
-import tempfile
 import time
 import unittest
-
 import warnings
 
 # By default, raise exceptions on warnings
@@ -30,50 +24,17 @@ warnings.simplefilter('error', FutureWarning)
 from influxdb import InfluxDBClient
 from influxdb.exceptions import InfluxDBClientError
 
-from tests.influxdb.misc import get_free_port, is_port_open
-from tests import skipIfPYpy, using_pypy
+from tests import skipIfPYpy, using_pypy, skipServerTests
+from tests.influxdb.server_tests.base import ManyTestCasesWithServerMixin
+from tests.influxdb.server_tests.base import SingleTestCaseWithServerMixin
 
 if not using_pypy:
     import pandas as pd
     from pandas.util.testing import assert_frame_equal
-    from influxdb import DataFrameClient
 
 
 THIS_DIR = os.path.abspath(os.path.dirname(__file__))
 
-#############################################################################
-# try to find where the 'influxd' binary is located:
-# You can define 'InfluxDbPythonClientTest_SERVER_BIN_PATH'
-#  env var to force it :
-influxdb_bin_path = influxdb_forced_bin = os.environ.get(
-    'InfluxDbPythonClientTest_SERVER_BIN_PATH', '')
-if not influxdb_bin_path:
-    try:
-        influxdb_bin_path = distutils.spawn.find_executable('influxd')
-        if not influxdb_bin_path:
-            raise Exception('not found via distutils')
-    except Exception as err:
-        try:
-            influxdb_bin_path = subprocess.check_output(
-                ['which', 'influxdb']).strip()
-        except subprocess.CalledProcessError as err:
-            # fallback on :
-            influxdb_bin_path = '/opt/influxdb/influxd'
-
-is_influxdb_bin_ok = (
-    # if the env var is set then consider the influxdb_bin as OK..
-    influxdb_forced_bin
-    or (os.path.isfile(influxdb_bin_path)
-        and os.access(influxdb_bin_path, os.X_OK))
-)
-
-if is_influxdb_bin_ok:
-    # read version :
-    version = subprocess.check_output([influxdb_bin_path, 'version'])
-    print(version, file=sys.stderr)
-
-
-#############################################################################
 
 def point(serie_name, timestamp=None, tags=None, **fields):
     res = {'measurement': serie_name}
@@ -152,174 +113,7 @@ dummy_point_without_timestamp = [
     }
 ]
 
-#############################################################################
-
-
-class InfluxDbInstance(object):
-    ''' A class to launch of fresh influxdb server instance
-    in a temporary place, using a config file template.
-    '''
-
-    def __init__(self, conf_template, udp_enabled=False):
-        # create a temporary dir to store all needed files
-        # for the influxdb server instance :
-        self.temp_dir_base = tempfile.mkdtemp()
-        # "temp_dir_base" will be used for conf file and logs,
-        # while "temp_dir_influxdb" is for the databases files/dirs :
-        tempdir = self.temp_dir_influxdb = tempfile.mkdtemp(
-            dir=self.temp_dir_base)
-        # we need some "free" ports :
-
-        ports = dict(
-            http_port=get_free_port(),
-            admin_port=get_free_port(),
-            meta_port=get_free_port(),
-            udp_port=get_free_port() if udp_enabled else -1,
-        )
-
-        conf_data = dict(
-            meta_dir=os.path.join(tempdir, 'meta'),
-            data_dir=os.path.join(tempdir, 'data'),
-            cluster_dir=os.path.join(tempdir, 'state'),
-            handoff_dir=os.path.join(tempdir, 'handoff'),
-            logs_file=os.path.join(self.temp_dir_base, 'logs.txt'),
-            udp_enabled='true' if udp_enabled else 'false',
-        )
-        conf_data.update(ports)
-        self.__dict__.update(conf_data)
-
-        conf_file = os.path.join(self.temp_dir_base, 'influxdb.conf')
-        with open(conf_file, "w") as fh:
-            with open(conf_template) as fh_template:
-                fh.write(fh_template.read().format(**conf_data))
-
-        # now start the server instance:
-        proc = self.proc = subprocess.Popen(
-            [influxdb_bin_path, '-config', conf_file],
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-        print("%s > Started influxdb bin in %r with ports %s and %s.." % (
-              datetime.datetime.now(),
-              self.temp_dir_base,
-              self.admin_port, self.http_port))
-
-        # wait for it to listen on the broker and admin ports:
-        # usually a fresh instance is ready in less than 1 sec ..
-        timeout = time.time() + 10  # so 10 secs should be enough,
-        # otherwise either your system load is high,
-        # or you run a 286 @ 1Mhz ?
-        try:
-            while time.time() < timeout:
-                if (is_port_open(self.http_port)
-                        and is_port_open(self.admin_port)):
-                    # it's hard to check if a UDP port is open..
-                    if udp_enabled:
-                        # so let's just sleep 0.5 sec in this case
-                        # to be sure that the server has open the port
-                        time.sleep(0.5)
-                    break
-                time.sleep(0.5)
-                if proc.poll() is not None:
-                    raise RuntimeError('influxdb prematurely exited')
-            else:
-                proc.terminate()
-                proc.wait()
-                raise RuntimeError('Timeout waiting for influxdb to listen'
-                                   ' on its ports (%s)' % ports)
-        except RuntimeError as err:
-            data = self.get_logs_and_output()
-            data['reason'] = str(err)
-            data['now'] = datetime.datetime.now()
-            raise RuntimeError("%(now)s > %(reason)s. RC=%(rc)s\n"
-                               "stdout=%(out)s\nstderr=%(err)s\nlogs=%(logs)r"
-                               % data)
-
-    def get_logs_and_output(self):
-        proc = self.proc
-        try:
-            with open(self.logs_file) as fh:
-                logs = fh.read()
-        except IOError as err:
-            logs = "Couldn't read logs: %s" % err
-        return {
-            'rc': proc.returncode,
-            'out': proc.stdout.read(),
-            'err': proc.stderr.read(),
-            'logs': logs
-        }
-
-    def close(self, remove_tree=True):
-        self.proc.terminate()
-        self.proc.wait()
-        if remove_tree:
-            shutil.rmtree(self.temp_dir_base)
-
-############################################################################
-
-
-def _setup_influxdb_server(inst):
-    inst.influxd_inst = InfluxDbInstance(
-        inst.influxdb_template_conf,
-        udp_enabled=getattr(inst, 'influxdb_udp_enabled', False))
-
-    inst.cli = InfluxDBClient('localhost',
-                              inst.influxd_inst.http_port,
-                              'root',
-                              '',
-                              database='db')
-    if not using_pypy:
-        inst.cliDF = DataFrameClient('localhost',
-                                     inst.influxd_inst.http_port,
-                                     'root',
-                                     '',
-                                     database='db')
-
-
-def _unsetup_influxdb_server(inst):
-    remove_tree = sys.exc_info() == (None, None, None)
-    inst.influxd_inst.close(remove_tree=remove_tree)
-
-############################################################################
-
-
-class SingleTestCaseWithServerMixin(object):
-    ''' A mixin for unittest.TestCase to start an influxdb server instance
-    in a temporary directory **for each test function/case**
-    '''
-
-    # 'influxdb_template_conf' attribute must be set
-    # on the TestCase class or instance.
-
-    setUp = _setup_influxdb_server
-    tearDown = _unsetup_influxdb_server
-
-
-class ManyTestCasesWithServerMixin(object):
-    ''' Same than SingleTestCaseWithServerMixin
-    but creates a single instance for the whole class.
-    Also pre-creates a fresh database: 'db'.
-    '''
-
-    # 'influxdb_template_conf' attribute must be set on the class itself !
-
-    @classmethod
-    def setUpClass(cls):
-        _setup_influxdb_server(cls)
-
-    def setUp(self):
-        self.cli.create_database('db')
-
-    @classmethod
-    def tearDownClass(cls):
-        _unsetup_influxdb_server(cls)
-
-    def tearDown(self):
-        self.cli.drop_database('db')
-
-############################################################################
-
-
-@unittest.skipIf(not is_influxdb_bin_ok, "could not find influxd binary")
+@skipServerTests
 class SimpleTests(SingleTestCaseWithServerMixin,
                   unittest.TestCase):
 
@@ -492,10 +286,7 @@ class SimpleTests(SingleTestCaseWithServerMixin,
                       ctx.exception.content)
 
 
-############################################################################
-
-
-@unittest.skipIf(not is_influxdb_bin_ok, "could not find influxd binary")
+@skipServerTests
 class CommonTests(ManyTestCasesWithServerMixin,
                   unittest.TestCase):
 
@@ -858,9 +649,7 @@ class CommonTests(ManyTestCasesWithServerMixin,
         self.cli.write_points(pts)
 
 
-############################################################################
-
-@unittest.skipIf(not is_influxdb_bin_ok, "could not find influxd binary")
+@skipServerTests
 class UdpTests(ManyTestCasesWithServerMixin,
                unittest.TestCase):
 
