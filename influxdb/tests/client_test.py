30b6447f2324d9cffc99838d5787cd4bc775cd5a
Georgi Dimitrov <georgijd92@gmail.com>
2015-09-13 15:14:26 +0000

    Fix PEP8 warning in client tests

--- influxdb/tests/client_test.py
+++ influxdb/tests/client_test.py
@@ -337,12 +337,13 @@ class TestInfluxDBClient(unittest.TestCase):
             cli.write_points_with_precision([])
 
     def test_query(self):
-        example_response = \
-            '{"results": [{"series": [{"measurement": "sdfsdfsdf", ' \
-            '"columns": ["time", "value"], "values": ' \
-            '[["2009-11-10T23:00:00Z", 0.64]]}]}, {"series": ' \
-            '[{"measurement": "cpu_load_short", "columns": ["time", "value"], ' \
+        example_response = (
+            '{"results": [{"series": [{"measurement": "sdfsdfsdf", '
+            '"columns": ["time", "value"], "values": '
+            '[["2009-11-10T23:00:00Z", 0.64]]}]}, {"series": '
+            '[{"measurement": "cpu_load_short", "columns": ["time", "value"], '
             '"values": [["2009-11-10T23:00:00Z", 0.64]]}]}]}'
+        )
 
         with requests_mock.Mocker() as m:
             m.register_uri(
