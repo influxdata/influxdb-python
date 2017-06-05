3edc76379ab0e9475534c4f65861b000648617eb
Sebastian Borza <sebito91@gmail.com>
2017-06-04 23:09:32 -0500

    Test on py35 and py36 (#458)

--- setup.py
+++ setup.py
@@ -50,6 +50,8 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ),
