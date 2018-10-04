e133e3ffb333c5a5361b54f2341aefb7b494af75
Shantanoo <shantanoo.desai@gmail.com>
2018-10-04 19:10:03 +0200

    Add Example for sending information to DB via UDP
     Due to lack of documentation for UDP, this example
     provides basic usage of sending information points
     via UDP. The code structure followed is similar, if
     not same as other examples in the `examples` directory.
    
     Locally tested with:
     InfluxDB v1.6.1 on Windows10 and influxdb-python v5.0.0
     and should not break anything
    
    Signed-off-by: Shantanoo <shantanoo.desai@gmail.com>

---
+++ examples/tutorial_udp.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""Example for sending batch information to InfluxDB via UDP"""
+
+"""
+INFO: In order to use UDP, one should enable the UDP service from the
+`influxdb.conf` under section
+    [[udp]]
+        enabled = true
+        bind-address = ":8089" # port number for sending data via UDP
+        database = "example-udp" # name of database to be stored
+"""
+
+
+import argparse
+
+from influxdb import InfluxDBClient
+
+def main(host='localhost', port=8086):
+    """Instantiate connection to the InfluxDB."""
+
+    # NOTE: structure of the UDP packet is different than that of information
+    #       sent via HTTP
+    json_body = {
+        "tags": {
+            "host": "server01",
+            "region": "us-west"
+        },
+        "time": "2009-11-10T23:00:00Z",
+        "points": [{
+            "measurement": "cpu_load_short",
+            "fields": {
+            "value": 0.64
+            }
+        },
+        {
+            "measurement": "cpu_load_short",
+            "fields": {
+            "value": 0.67
+            }
+        }
+        ]
+    }
+
+    # make `use_udp` True and  add `udp_port` number from `influxdb.conf` file
+    # no need to mention the database name since it is already configured
+    client = InfluxDBClient(host, port, use_udp=True, udp_port=8089)
+
+    # Instead of `write_points` use `send_packet`
+    client.send_packet(json_body)
+
+def parse_args():
+    """Parse the args."""
+    parser = argparse.ArgumentParser(
+        description='example code to play with InfluxDB')
+    parser.add_argument('--host', type=str, required=False,
+                        default='localhost',
+                        help='hostname of InfluxDB http API')
+    parser.add_argument('--port', type=int, required=False, default=8086,
+                        help='port of InfluxDB http API')
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(host=args.host, port=args.port)
