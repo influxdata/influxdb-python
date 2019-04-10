c7425c0ac7ecb565a8d585ce509ba40dc8df5fbf
Shantanoo <shantanoo.desai@gmail.com>
2019-04-10 13:11:21 +0200

    Improve `tutorial_udp.py` example
    - Instead of providing arguments of HTTP InfluxDB,
      provide UDP Port as argument
    - Improve the Comments to distinguish between different
      UDP Ports (e.g. 8089(default) saves to `udp1` and 8090
      saves to `udp2` databases respectively
    
    Signed-off-by: Shantanoo <shantanoo.desai@gmail.com>

--- examples/tutorial_udp.py
+++ examples/tutorial_udp.py
@@ -7,7 +7,11 @@ INFO: In order to use UDP, one should enable the UDP service from the
     [[udp]]
         enabled = true
         bind-address = ":8089" # port number for sending data via UDP
-        database = "example-udp" # name of database to be stored
+        database = "udp1" # name of database to be stored
+    [[udp]]
+        enabled = true
+        bind-address = ":8090"
+        database = "udp2"
 """
 
 
@@ -15,7 +19,8 @@ import argparse
 
 from influxdb import InfluxDBClient
 
-def main(host='localhost', port=8086):
+
+def main(uport):
     """Instantiate connection to the InfluxDB."""
 
     # NOTE: structure of the UDP packet is different than that of information
@@ -29,36 +34,34 @@ def main(host='localhost', port=8086):
         "points": [{
             "measurement": "cpu_load_short",
             "fields": {
-            "value": 0.64
+                "value": 0.64
             }
         },
-        {
-            "measurement": "cpu_load_short",
-            "fields": {
-            "value": 0.67
-            }
-        }
-        ]
+            {
+                "measurement": "cpu_load_short",
+                "fields": {
+                    "value": 0.67
+                }
+        }]
     }
 
     # make `use_udp` True and  add `udp_port` number from `influxdb.conf` file
     # no need to mention the database name since it is already configured
-    client = InfluxDBClient(host, port, use_udp=True, udp_port=8089)
+    client = InfluxDBClient(use_udp=True, udp_port=uport)
 
     # Instead of `write_points` use `send_packet`
     client.send_packet(json_body)
 
+
 def parse_args():
     """Parse the args."""
     parser = argparse.ArgumentParser(
-        description='example code to play with InfluxDB')
-    parser.add_argument('--host', type=str, required=False,
-                        default='localhost',
-                        help='hostname of InfluxDB http API')
-    parser.add_argument('--port', type=int, required=False, default=8086,
-                        help='port of InfluxDB http API')
+        description='example code to play with InfluxDB along with UDP Port')
+    parser.add_argument('--uport', type=int, required=True,
+                        help=' UDP port of InfluxDB')
     return parser.parse_args()
+
 
 if __name__ == '__main__':
     args = parse_args()
-    main(host=args.host, port=args.port)
+    main(uport=args.uport)
