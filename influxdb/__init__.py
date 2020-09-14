ecbabcc41efc644f450700943bf450b07822fbae
doyle <doyle@arista.com>
2020-09-14 12:25:30 -0700

    Allow skipping DataFrameClient import
    Importing the DataFrameClient can take quite a while,
    especially when the source files aren't yet loaded into
    the page cache.
    
    This patch adds an environment variable, INFLUXDB_NO_DATAFRAME_CLIENT,
    to allow users of this package to skip this import in cases where
    they don't need it.

--- influxdb/__init__.py
+++ influxdb/__init__.py
@@ -6,16 +6,20 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import os
+
 from .client import InfluxDBClient
-from .dataframe_client import DataFrameClient
 from .helper import SeriesHelper
 
 
 __all__ = [
     'InfluxDBClient',
-    'DataFrameClient',
     'SeriesHelper',
 ]
+
+if "INFLUXDB_NO_DATAFRAME_CLIENT" not in os.environ:
+   from .dataframe_client import DataFrameClient
+   __all__.append( "DataFrameClient" )
 
 
 __version__ = '5.3.0'
