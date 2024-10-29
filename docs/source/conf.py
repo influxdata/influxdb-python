bbe80ed32cf57b252be131d3edcda5ca610fc223
Jason Stirnaman <stirnamanj@gmail.com>
2024-10-29 10:54:14 -0500

    Update docs (#929)
    * chore(docs): Update version advice and tox build requirements:
    
    - Updates the version advice and removes the succession statement to be consistent with the OSS 1.11 release.
    - Adds explicit relations to v2 and v3.
    - Removes redundancies.
    - Fixes formatting errors reported by the linter.
    - Updates dependencies for docs build.
    
    * fix(docs): mention 1.x compat endpoints in 2.x

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -117,7 +117,8 @@ html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# Calling get_html_theme_path is deprecated.
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
